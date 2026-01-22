fa986e96ba5c47a5a54ed7d5f05111bff73abfb4
Michelle <bontlemichelle66@gmail.com>
2026-01-22 02:01:13 -0500

    updating

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
          view=views.get_cars, name='getcars'),
 
     # path for dealer reviews view
-    path(route='get_dealers',
+    path(route='get_dealers/',
          view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>',
          view=views.get_dealerships, name='get_dealers_by_state'),
