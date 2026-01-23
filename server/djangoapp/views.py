65c02c5e613449d245f78cc525fff3a9baabde86
Michelle <99816494+Michellerams@users.noreply.github.com>
2026-01-23 07:50:47 +0200

    Fix indentation in get_dealer_reviews function

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -105,6 +105,7 @@ def get_dealer_details(request, dealer_id):
         #return JsonResponse({"status": 200, "reviews": reviews})
     #else:
         #return JsonResponse({"status": 400, "message": "Bad Request"})
+
 def get_dealer_reviews(request, dealer_id):
     if not dealer_id:
         return JsonResponse({"status": 400, "message": "Bad Request"})
